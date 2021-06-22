e23245f0407e7a41ff0e00bd0d7cd9ce658e8628
cneralich <corrigan@hashicorp.com>
2021-06-22 14:47:03 -0700

    Add files via upload

---
+++ sub-1/main.tf
@@ -0,0 +1,20 @@
+resource "random_pet" "server" {
+  length = var.pet_name_length
+  prefix = var.prefix
+}
+
+# variables.tf
+
+variable "pet_name_length" {
+  type    = string
+  default = "1"
+}
+
+variable "prefix" {
+  default = "test"
+}
+
+# output.tf
+output "server" {
+  value = random_pet.server.id
+}
