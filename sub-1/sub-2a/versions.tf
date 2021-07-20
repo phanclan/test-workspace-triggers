7a87406cfc852027cdeda7c6d91a0d5738bd15f7
Peter Phan <pphan@hashicorp.com>
2021-07-20 09:59:56 -0700

    Update sub-2a 202107200959

---
+++ sub-1/sub-2a/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  backend "remote" {
+    organization = "pphan"
+
+    workspaces {
+      name = "sub-2a"
+    }
+  }
+}
+output "workspace" {
+  value = "sub-2a 202107200959"
+}
